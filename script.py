c3281bdf13a29a5ef3842c35a59044c6d70191cb
Mateo Jurado <rjuadoj@est.ups.edu.ec>
2026-05-04 15:32:40 -0500

    Mi primer subida

---
+++ script.py
@@ -0,0 +1,12 @@
+#---------------------------------------------
+#Script de python para probar en Git
+#Autor: Mateo Jurado <rjuradoj@est.ups.edu.ec>
+#Fecha: 04/05/2026
+#---------------------------------------------
+
+def main():
+    nombre= input("Como te llamas: ")
+    print("Hola", nombre, "Bienvenido a mi mundo")
+
+if __name__ == "__main__":
+    main()
